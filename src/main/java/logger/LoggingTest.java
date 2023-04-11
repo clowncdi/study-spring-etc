f317c822b283d2a7b7bf88d473c14a6f54e90ab6
clowncdi <clowncdi85@gmail.com>
2023-04-11 15:06:42 +0900

    feat: Slf4j 예제 추가

--- src/main/java/logger/LoggingTest.java
+++ src/main/java/logger/LoggingTest.java
@@ -5,12 +5,19 @@ import java.util.logging.Logger;
 
 public class LoggingTest {
 	public static Logger logger = Logger.getLogger(LoggingTest.class.getName());
+	public static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(LoggingTest.class); // Slf4j
 
 	public static void main(String[] args) {
 
 		logger.log(Level.WARNING, "Warning logging");
 		logger.log(Level.INFO, "Info logging");
 		logger.log(Level.SEVERE, "Severe logging");
+
+		log.trace("trace logging");
+		log.debug("debug logging");
+		log.info("info logging");
+		log.warn("warning logging");
+		log.error("error logging");
 
 	}
 }
