ecda118f6654e75c850fd47c91d0fea01ca0ce62
clowncdi <clowncdi85@gmail.com>
2023-04-17 14:17:30 +0900

    feat: 기본 로그 예제 만들기

---
+++ src/main/java/log/LogApplication.java
@@ -0,0 +1,14 @@
+package log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 인프런 김영한 스프링 핵심원리 - 고급편
+ */
+@SpringBootApplication
+public class LogApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LogApplication.class, args);
+	}
+}
---
+++ src/main/java/log/app/v2/v1/OrderServiceV2.java
@@ -0,0 +1,29 @@
+package log.app.v2.v1;
+
+import org.springframework.stereotype.Service;
+
+import log.hellotrace.HelloTraceV2;
+import log.trace.TraceId;
+import log.trace.TraceStatus;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+	private final OrderRepositoryV2 orderRepository;
+	private final HelloTraceV2 trace;
+
+	public void orderItem(TraceId traceId, String itemId) {
+
+		TraceStatus status = null;
+		try {
+			status = trace.beginSync(traceId, "OrderService.orderItem()");
+			orderRepository.save(status.getTraceId(), itemId);
+			trace.end(status);
+		} catch (Exception e) {
+			trace.exception(status, e);
+			throw e;
+		}
+	}
+}
